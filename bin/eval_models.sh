797671f32f01fd8737e1e42d13831711910dcf06
chemgeeklian <xinran.lian@gmail.com>
2025-10-23 21:34:30 +0000

    Remove bin/ from tracking and ignore it going forward

--- bin/eval_models.sh
+++
@@ -1,9 +0,0 @@
-declare -a models=( "esm1b" "esm1v1" "esm1v2" "esm1v3" "esm1v4" "esm1v5" )
-
-for model in "${models[@]}"
-do
-    echo $model
-    python bin/$1.py --model-name $model > reconstruct_$model.log
-done
-
-bash bin/grep_esm1v.sh .
--- bin/make_fasta.sh
+++
@@ -1,9 +0,0 @@
-line_num=0
-
-cat $1 | \
-    while read line
-    do
-        echo ">seq"$line_num
-        echo $line
-        ((line_num+=1))
-    done
